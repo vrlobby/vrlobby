d026b7b216dfd9a4505edc7ecebf1d5730964fdd
EC2 Default User <ec2-user@ip-172-31-35-241.ec2.internal>
2017-07-11 01:27:41 +0000

    Updated to do 15 min check and push to table

--- getandpushcounts.sh
+++ getandpushcounts.sh
@@ -6,12 +6,18 @@
 # This script also requires that you have a dynamodb table variable named "COUNTS_TABLE",
 # and that whatever you're running this from is permissioned to write to it.
 
+source ./setenv.sh
+
 # Get most recent file from appids directory
 appids=$(ls -dt ./appids/* | head -1)
 
 fileDate=$(date +"%m-%d-%y_%H-%M")
-outFile="./counts/$date.counts"
-date="$( date +%s )" #seconds since epoch
+outFile="./counts/$fileDate.counts"
+datetime="$( date +%s )" #seconds since epoch
+date=$(date +"%Y%m%d")
+hour=$(date +"%H")
+mins=$(date +"%M")
+quarter=$(( $mins / 15 ))
 
 while read appid; do
 	count="$(curl -s "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key=$STEAMKEY&format=json&appid=$appid" | sed -n 's/.*count": \(.*\+\),/\1/p' )"
@@ -19,6 +25,6 @@ while read appid; do
 done <$appids >$outFile
 
 counts="$( cat $outFile )"
-item="{\"date\": {\"N\": \"$date\"}, \"counts\": {\"S\": \"$counts\"} }"
+item="{\"date\": { \"S\": \"$date\"}, \"datetime\": { \"N\": \"$datetime\"}, \"hour\": { \"N\": \"$hour\"}, \"quarter\": { \"N\": \"$quarter\"}, \"counts\": {\"S\": \"$counts\"} }"
 aws dynamodb put-item --table-name "$COUNTS_TABLE" --item "$item"
 
