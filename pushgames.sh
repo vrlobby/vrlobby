24710f1530f94f5c87221bf4afd21ac92ec88ec8
EC2 Default User <ec2-user@ip-172-31-35-241.ec2.internal>
2019-04-09 19:29:01 +0000

    Fixed a couple things due to a change a while back in the steam pages

--- pushgames.sh
+++ pushgames.sh
@@ -2,7 +2,9 @@
 
 FILE=$1
 
-while read appid; do
-	item="{\"appid\": {\"N\": \"$appid\"}}"
+while read appidAndName; do
+	appid=${appidAndName%%|*}
+	name=${appidAndName#*|}
+	item="{\"appid\": {\"N\": \"$appid\"}, \"name\": {\"S\": \"$name\"}}"
 	aws dynamodb put-item --table-name multiplayer-games --item "$item"
 done <$FILE
